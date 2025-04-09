4a8dbd7ee6ee39e3b942757f5db7daf5b710f800
danilearner <dakhmad140104@gmail.com>
2025-04-09 20:44:46 +0700

    Still didn't understand haha

---
+++ r1-basic-pcd.py
@@ -0,0 +1,10 @@
+'''
+    Program : Review Pengolahan Citra Digital
+    About   : Refreshing PCD
+    Tanggal : 09 April 2025
+'''
+
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
